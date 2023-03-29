c31b070352d5f671604b67f5225aaefc225d3599
KamilTheKoder <kamilkazmierczak0611@gmail.com>
2023-03-29 16:21:20 +0200

    fixed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CryptoModule } from "./crypto/crypto.module";
 
@@ -13,7 +12,6 @@ import { CryptoModule } from "./crypto/crypto.module";
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
-        AppRoutingModule,
         CryptoModule
     ]
 })
